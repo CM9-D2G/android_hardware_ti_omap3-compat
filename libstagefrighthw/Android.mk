16c6455eed77c18659a6155b6c5bce7d59e8e1fe
Jonathan Bither <jonbither@gmail.com>
2012-07-12 11:05:37 -0400

    libstagefrighthw: sync with omapzoom

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -7,16 +7,16 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
 
 LOCAL_C_INCLUDES:= \
-    $(ANDROID_BUILD_TOP)/frameworks/base/include/media/stagefright/openmax
+        $(TOP)/frameworks/base/include/media/stagefright/openmax
 
-LOCAL_SHARED_LIBRARIES := \
-    libbinder \
-    libutils  \
-    libcutils \
-    libui     \
-    libdl     \
+LOCAL_SHARED_LIBRARIES :=       \
+        libbinder               \
+        libutils                \
+        libcutils               \
+        libui                   \
+        libdl                   \
 
 LOCAL_MODULE := libstagefrighthw
-
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
